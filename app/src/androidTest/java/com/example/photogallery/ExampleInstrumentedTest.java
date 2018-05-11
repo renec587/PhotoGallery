0ac9057dcce53b5f38b09237f114628283ab2dba
renec587 <38676245+renec587@users.noreply.github.com>
2018-05-10 22:18:40 -0700

    Added simple junit test

--- app/src/androidTest/java/com/example/photogallery/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/photogallery/ExampleInstrumentedTest.java
@@ -1,11 +1,16 @@
 package com.example.photogallery;
 
 import android.content.Context;
+import android.media.ExifInterface;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
@@ -16,16 +21,45 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    private DiskFiles testFiles;
+
     @Test
+    @Before
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 //Changes to see if git works.
         assertEquals("com.example.photogallery", appContext.getPackageName());
-        DiskFiles testFiles = new DiskFiles(appContext);
+        testFiles = new DiskFiles(appContext);
     }
-    @Test
-    public void testmethod() {
 
+    @Test
+    public void testCaptions() {
+        File testFile;
+        testFile = testFiles.get();
+        assertEquals("Home",getCaption(testFile));
+        testFiles.previous();
+        testFile = testFiles.get();
+        assertEquals("Kitchen",getCaption(testFile));
+        testFiles.previous();
+        testFile = testFiles.get();
+        assertEquals("BookCase",getCaption(testFile));
+        testFiles.previous();
+        testFile = testFiles.get();
+        assertEquals("Cat",getCaption(testFile));
+        testFiles.previous();
+        testFile = testFiles.get();
+        assertEquals("Home",getCaption(testFile));
+    }
+
+    public String getCaption(File thisFile) {
+        ExifInterface exif;
+        try {
+            exif = new ExifInterface(thisFile.getPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+        return exif.getAttribute(ExifInterface.TAG_IMAGE_DESCRIPTION);
     }
 }
